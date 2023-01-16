1cf120e39637add1207d7b1fb07409677d8af516
Franklin Lainez <franklin@returnsafe.com>
2023-01-16 16:33:54 -0600

    Add post handler

--- server.js
+++ server.js
@@ -15,8 +15,34 @@ const friends = [
   },
 ];
 
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+
+  // actions go here...
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+app.use(express.json());
+
 app.get('/friends', (req, res) => {
   res.json(friends);
+});
+
+app.post('/friends', (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: 'Missing friend name',
+    });
+  }
+
+  const newFriend = {
+    name: req.body.name,
+    id: friends.length,
+  };
+  friends.push(newFriend);
+  res.json(newFriend);
 });
 
 app.get('/friends/:friendId', (req, res) => {
