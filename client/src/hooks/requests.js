08fde7269d0500bf2f5205535757e467a0b6f773
Franklin Lainez <franklin@returnsafe.com>
2023-01-17 10:49:55 -0600

    Implement getAllPlanets endpoint

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,6 +1,8 @@
+const URL = 'http://localhost:8000';
+// Load planets
 async function httpGetPlanets() {
-  // TODO: Once API is ready.
-  // Load planets and return as JSON.
+  const response = await fetch(`${URL}/planets`);
+  return response.json();
 }
 
 async function httpGetLaunches() {
@@ -18,9 +20,4 @@ async function httpAbortLaunch(id) {
   // Delete launch with given ID.
 }
 
-export {
-  httpGetPlanets,
-  httpGetLaunches,
-  httpSubmitLaunch,
-  httpAbortLaunch,
-};
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,41 @@
-const planets = [];
+const fs = require('fs');
+const path = require('path');
+const { parse } = require('csv-parse');
 
-module.exports = planets;
+const habitablePlanets = [];
+
+const isHabitablePlanet = (planet) =>
+  planet.koi_disposition === 'CONFIRMED' &&
+  planet.koi_insol > 0.36 &&
+  planet.koi_insol < 1.11 &&
+  planet.koi_prad < 1.6;
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, '..', '..', 'data', 'kepler_data.csv')
+    )
+      .pipe(
+        parse({
+          comment: '#',
+          columns: true
+        })
+      )
+      .on('data', (data) => {
+        if (isHabitablePlanet(data)) habitablePlanets.push(data);
+      })
+      .on('error', () => {
+        console.log('error');
+        reject();
+      })
+      .on('end', () => {
+        console.log('done', habitablePlanets.length);
+        resolve();
+      });
+  });
+}
+
+module.exports = {
+  planets: habitablePlanets,
+  loadPlanetsData
+};
